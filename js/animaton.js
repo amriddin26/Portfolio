5e70b335ab96070b735e5af10abafc0b197da773
amriddin06 <amriddinjorayev7@gmail.com>
2023-08-30 15:25:08 +0500

    efrtsdf

--- js/animaton.js
+++ js/animaton.js
@@ -1,20 +1,20 @@
     const circles1 = document.querySelectorAll('.circle1');
-    circles1.forEach(elem=>{
-    var dots = elem.getAttribute("data-dots");
-    var marked = elem.getAttribute("data-percent");
-    var percent = Math.floor(dots*marked/100);
-    var points = "";
-    var rotate = 360 / dots; 
+    circles1.forEach(elem => {
+        var dots = elem.getAttribute("data-dots");
+        var marked = elem.getAttribute("data-percent");
+        var percent = Math.floor(dots * marked / 100);
+        var points = "";
+        var rotate = 360 / dots;
 
 
-    for(let i = 0 ; i < dots ; i++){
-        points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
-    } 
-    elem.innerHTML = points;
+        for (let i = 0; i < dots; i++) {
+            points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
+        }
+        elem.innerHTML = points;
 
 
-    const pointsMarked = elem.querySelectorAll('.points');
-    for(let i = 0; i < percent ; i++){
-        pointsMarked[i].classList.add('marked');
-    }
-});
+        const pointsMarked = elem.querySelectorAll('.points');
+        for (let i = 0; i < percent; i++) {
+            pointsMarked[i].classList.add('marked');
+        }
+    });
--- js/script.js
+++ js/script.js
@@ -1,44 +1,39 @@
 let words = document.querySelectorAll(".word");
-words.forEach((word)=>{
+words.forEach((word) => {
     let letters = word.textContent.split("");
-    word.textContent="";
-    letters.forEach((letters)=>{
-       let span = document.createElement("span");
-       span.textContent = letters;
-       span.className = "letter";
-       word.append(span);
+    word.textContent = "";
+    letters.forEach((letters) => {
+        let span = document.createElement("span");
+        span.textContent = letters;
+        span.className = "letter";
+        word.append(span);
     });
 });
 
 let currentWordIndex = 0;
-let maxWordIndex = words.length -1;
-words[currentWordIndex].style.opacity ="1";
+let maxWordIndex = words.length - 1;
+words[currentWordIndex].style.opacity = "1";
 
 
-let chandeText = () =>{
+let chandeText = () => {
     let currentWord = words[currentWordIndex];
     let nextWord = currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
 
 
-   Array.from(currentWord.children).forEach((letters,i)=>{
-       setTimeout(()=>{
-        letters.className = "letter out";
-       },i * 80);
-       nextWord.style.opacity="1";
-       Array.from(nextWord.children).forEach((letter,i)=>{
-        letter.className = "letter behind";
-        setTimeout(()=>{
-            letter.className = "letter in";
-        },340 + i * 80);
-       });
-   });
-   currentWordIndex = currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
+    Array.from(currentWord.children).forEach((letters, i) => {
+        setTimeout(() => {
+            letters.className = "letter out";
+        }, i * 80);
+        nextWord.style.opacity = "1";
+        Array.from(nextWord.children).forEach((letter, i) => {
+            letter.className = "letter behind";
+            setTimeout(() => {
+                letter.className = "letter in";
+            }, 340 + i * 80);
+        });
+    });
+    currentWordIndex = currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
 };
 
 chandeText();
-setInterval(chandeText,3000);
-
-// var mixer = mixitu  ///////////////////
-
-var mixer = mixitup('.portfolio-gellery');
-var mixer = mixitup('.portfolio-gellery');
+setInterval(chandeText, 3000);
